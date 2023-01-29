7722228838764b638773082aa8af4358d44f61e4
Mitchell <mitchellwong833@gmail.com>
2023-01-30 12:56:36 +1300

    Registered the service in program.cs

--- BudgetTrackerAPI/Controllers/TransactionController.cs
+++ BudgetTrackerAPI/Controllers/TransactionController.cs
@@ -1,5 +1,4 @@
-﻿using BudgetTrackerAPI.Services.TransactionService;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BudgetTrackerAPI.Controllers
--- BudgetTrackerAPI/Program.cs
+++ BudgetTrackerAPI/Program.cs
@@ -1,5 +1,6 @@
 global using BudgetTrackerAPI.Models;
 global using BudgetTrackerAPI.Enums;
+global using BudgetTrackerAPI.Services.TransactionService;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ITransactionService, TransactionService>();
 
 var app = builder.Build();
 
